03bcd808ad010c9d8c9f06e43a0aefae781deb3a
Rahul <vaish.rahul4810@gmail.com>
2020-06-18 23:28:43 +0530

    osm-nominatim

--- leaflet-search-master/html/search.php
+++ leaflet-search-master/html/search.php
@@ -1,53 +1,4 @@
 <?php
-/*
- * Demo serverside file for Leaflet Search Plugin
- * https://github.com/stefanocudini/leaflet-search
- *
- * http://labs.easyblog.it/maps/leaflet-search
- *
- * Copyright 2013, Stefano Cudini - http://labs.easyblog.it/stefano-cudini/
- * Licensed under the MIT license.
- 
- What's:
-	 php code for testing jsonp and ajax features
-	 
-	 receive get parameters:
-	 	q		 :	search text
-	 	callback :	callback name for jsonp request
-
- Example Ajax:
- 	request:
- 		search.php?q=dark
- 	response:
-		[{"loc":[41.34419,13.242145],"title":"darkblue"},{"loc":[41.67919,13.122145],"title":"darkred"}]
-
- Example Jsonp:
- 	request:
- 		search.php?q=dark&callback=L.Control.Search.callJsonp
- 	response:
-		L.Control.Search.callJsonp([{"loc":[41.34419,13.242145],"title":"darkblue"},{"loc":[41.67919,13.122145],"title":"darkred"}])
-
- Example Bulk data:
- 	request:
- 		search.php?q=roma&cities=1
- 	response:
-		[{"title":"Romainville","loc":[48.8854,2.43482]},{"title":"Roma","loc":[41.89474,12.4839]},{"title":"Roman","loc":[46.91667,26.91667]}]
-
-
- Example Ajax Empty Result:
- 	request:
- 		search.php?q=xx
- 	response:
-		{"ok":1,"results":[]}
-
- Example Error Result:
- 	request:
- 		search.php?s=dark
- 	response:
-		{"ok":0,"errmsg":"specify query parameter"}
-
-*/
-
 if(!isset($_GET['q']) or empty($_GET['q']))
 	die( json_encode(array('ok'=>0, 'errmsg'=>'specify q parameter') ) );
 
@@ -100,6 +51,5 @@ if(isset($_GET['callback']) and !empty($_GET['callback']))	//support for JSONP r
 	echo $_GET['callback']."($JSON)";
 else
 	echo $JSON;	//AJAX request
-
 
 ?>
